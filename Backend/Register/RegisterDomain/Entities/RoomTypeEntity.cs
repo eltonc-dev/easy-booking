a6f3c214478302dfa1c0c4564fa10cae8c21a308
Elton Coelho <eltonc.dev@gmail.com>
2023-07-01 19:15:45 -0300

    ïnitial

---
+++ Backend/Register/RegisterDomain/Entities/RoomTypeEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace RegisterDomain.Entities
+{
+	public class RoomEntity
+	{
+		public Guid Id { get; set; }
+		public int Number { get; set; }
+        public int Flor { get; set; }
+        public Guid RoomTypeId { get; set; }
+        public string? Description { get; set; }
+
+        public RoomEntity(Guid id, int number, int flor, Guid roomType)
+		{
+			Id = id;
+			Number = number;
+			Flor = flor;
+			RoomTypeId = roomType;
+		}
+	}
+}
+
---
+++ Backend/Register/RegisterWeb/Controllers/RoomsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace RegisterWeb.Controllers
+{
+    [ApiController]
+    [Route("Rooms")]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new List<string>());
+        }
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+            return Ok(new List<string>());
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public IActionResult Edit(string id)
+        {
+            return Ok(new List<string>());
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            return Ok(new List<string>());
+        }
+    }
+}
+
