c1759fa56588f6f52329823535b68c1f66071812
Elton Coelho <eltonc.dev@gmail.com>
2023-07-01 19:15:45 -0300

    ïnitial

---
+++ Backend/Register/RegisterDomain/Entities/RoomEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RegisterDomain.Entities
+{
+	public class RoomTypeEntity
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Acronym { get; set; }
+
+        public RoomTypeEntity(Guid id, string name, string acronym)
+		{
+			Id = id;
+			Name = name;
+			Acronym = acronym;
+		}
+	}
+}
+
---
+++ Backend/Register/RegisterWeb/Controllers/RoomTypesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RegisterDomain.Entities;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RegisterWeb.Controllers
+{
+    [ApiController]
+    [Route("RoomTypes")]
+    public class RoomTypesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllAsync()
+        {
+            return Ok(new List<RoomTypeEntity>()); ;
+        }
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+            return Ok(new List<string>());
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public IActionResult Edit(string id)
+        {
+            return Ok(new List<string>());
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            return Ok(new List<string>());
+        }
+    }
+}
+
